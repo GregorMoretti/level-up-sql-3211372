48a12872d9c3256b40692169f66acfaa5beab5b7
GregorMoretti <61017085+GregorMoretti@users.noreply.github.com>
2022-11-23 15:51:11 +0000

    test

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,4 @@
 -- Generate a list of customer information.
-
+-- so ein Kommentar von mir zum Test
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
